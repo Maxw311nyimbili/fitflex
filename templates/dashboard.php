612484994cc46d4adfdcf8aedf11ca51b37ffa38
Maxw311 <nyimbilimaxwell9@gmail.com>
2024-11-22 13:38:39 +0000

    minor update

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'db_connect.php'; // Assuming you have a 'db_connect.php' file for MySQL connection
+require 'db_connect.php'; 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
