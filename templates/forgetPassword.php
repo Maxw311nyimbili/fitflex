153f9c1fac4013d85ed69def812dc2c53af82c6a
Maxw311 <nyimbilimaxwell9@gmail.com>
2024-11-25 18:57:44 +0000

    minor-adjustment

--- templates/forgetPassword.php
+++ templates/forgetPassword.php
@@ -3,6 +3,11 @@
 require '../vendor/autoload.php'; // Adjust path if necessary
 include 'db_connect.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
